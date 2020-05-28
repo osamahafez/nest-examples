e7c4cb227ddd5e48716cc5ba4f86027230ce9b89
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2020-05-28 11:41:30 +0200

    feat: user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,29 @@
 import { Module } from '@nestjs/common';
+import { Connection } from 'typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nest-blog-api',
+      entities: [User],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
     UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
