8d4709404df1e7b5c07cb23c9871983f4e6280a0
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2020-05-30 10:00:08 +0200

    clean up to service

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,10 +1,13 @@
 import { Resolver, Query } from '@nestjs/graphql';
+import { UsersService } from './users.service';
 
 @Resolver('Users')
 export class UsersResolver {
     
+    constructor(private readonly usersService: UsersService) {}
+
     @Query(() => String)
     async helloGraphql() {
-        return 'hello graphQL';
+        return this.usersService.helloGraphql();
     }
 }
