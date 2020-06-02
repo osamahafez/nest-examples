217866ba2e2297254593727da54deb95167cc60d
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2020-06-02 14:36:07 +0200

    change require to import

--- src/payment/credit-card/credit-card.service.ts
+++ src/payment/credit-card/credit-card.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@nestjs/common';
-const Stripe = require('stripe');
+import Stripe from 'stripe';
 
 @Injectable()
 export class CreditCardService {
 
     async checkout() {
 
-        const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+        // initialize stripe instance with api secret key and the api version (found in stripe dashboard under developers section)
+        const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+            apiVersion: '2020-03-02'
+        });
 
+        // set item price and currency
         const paymentIntent = await stripe.paymentIntents.create({
             amount: 1099,
             currency: 'usd',
         });
           
+        // send client secret to frontend to handle credit card payments
         return {client_secret: paymentIntent.client_secret};
     }
 }
